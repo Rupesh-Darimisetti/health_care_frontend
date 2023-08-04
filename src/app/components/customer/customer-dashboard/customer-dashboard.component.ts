44b816c923006366c695cb4864677f21dd6bf6cc
irsayvid <dsdarimisetti@gmail.com>
2023-08-04 23:23:41 +0530

    add customer dashboard table

--- src/app/components/customer/customer-dashboard/customer-dashboard.component.ts
+++ src/app/components/customer/customer-dashboard/customer-dashboard.component.ts
@@ -1,13 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { filter, map } from 'rxjs/operators';
+/** TODO: replace interface with available hospitals after modifying service */
+import { HospitalAccount } from 'src/app/interfaces/HospitalAccount';
+import { HospitalAccountService } from 'src/app/service/hospital-account.service';
 
 @Component({
   selector: 'app-customer-dashboard',
   templateUrl: './customer-dashboard.component.html',
-  styleUrls: ['./customer-dashboard.component.css']
+  styleUrls: ['./customer-dashboard.component.css'],
 })
 export class CustomerDashboardComponent implements OnInit {
-  constructor() { }
-  ngOnInit(): void {
+  HospitalsCount: number = 0;
+  displayedColumns: String[] = [
+    'h_id',
+    'name',
+    'location',
+    'contact_num',
+    'email',
+    'website',
+    '',
+  ];
+  dataSource!: MatTableDataSource<HospitalAccount>;
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    private dialog: MatDialog,
+    private hospitalAccountService: HospitalAccountService
+  ) {}
+  ngOnInit(): void {
+    this.getAvailableHospitals();
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  /** TODO: Use filter/map to get hospitals with available beds count > 0 */
+  getAvailableHospitals(): void {
+    this.hospitalAccountService.getHospitalAccount().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: (err) => {
+        alert('error while getting available beds');
+      },
+    });
   }
 }
--- src/app/interfaces/HospitalAccount.ts
+++ src/app/interfaces/HospitalAccount.ts
@@ -1,16 +1,19 @@
 export interface HospitalAccount {
+  h_id: number;
+  h_name: string;
+  h_password: string;
+  h_street: string;
+  h_zip_code: number;
+  h_city: string;
+  h_state: string;
+  h_contact_number: string;
+  h_email: string;
+  h_website?: String;
+  h_lab_facility1?: String;
+  h_scanning_facility?: String;
+  h_insurance_acceptance?: String;
+}
 
-    h_id: number;
-    h_name: string;
-    h_password: string;
-    h_street: string;
-    h_zip_code: number;
-    h_city: string;
-    h_state: string;
-    h_contact_number: string;
-    h_email: string;
-    h_website?: String;
-    h_lab_facility1?: String;
-    h_scanning_facility?: String;
-    h_insurance_acceptance?: String;
+export interface AvailableHospitals extends HospitalAccount {
+  beds_available: Number;
 }
